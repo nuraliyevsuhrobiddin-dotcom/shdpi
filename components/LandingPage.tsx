5106a2d2e2318098864af16d7d1af2a90b54ed66
nuraliyevsuhrobiddin-dotcom <nuraliyevsuhrobiddin@gmail.com>
2026-02-13 10:05:26 +0300

    Create LandingPage.tsx

---
+++ components/LandingPage.tsx
@@ -0,0 +1,8 @@
+export default function LandingPage() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>BIOXATO.AI</h1>
+      <p>Laboratory Protocol Analyzer</p>
+    </div>
+  );
+}
